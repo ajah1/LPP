9fc3a401ff9f74950fca3df4d70512c329f73002
nyaboron <ajah1@alu.ua.es>
2018-02-13 09:30:14 +0100

    p02 cambiada

--- p02.rkt
+++ p02.rkt
@@ -4,70 +4,49 @@
         (schemeunit))
 
 ;----------------------------Actividad 1
-(define (cuenta-simbolos l)
-  (cond
-    ((null? l) 0)
-    ((symbol? (car l)) (+ 1 (cuenta-simbolos (cdr l))))
-    (else (cuenta-simbolos (cdr l)))))
-
-;; Demostracion
-(display "\nActividad 1")
-(display "\ncuenta-simbolos '(2 40 #f) => ")
-(display (cuenta-simbolos '(2 40 #f) ))
-;; Pruebas
-(check-eq?  (cuenta-simbolos '(2 4 #f a)) 1)
-(check-eq?  (cuenta-simbolos '(2 a "hola" #f hola)) 2)
-
-;----------------------------Actividad 2
 (define (menor x y)
   (if (< x y) x y))
 
 (define (mayor x y)
   (if (> x y) x y))
 
-(define (minimo lista)
+(define (maximo lista)
   (cond
     ((null? (cdr lista)) (car lista))
-    (else (menor (car lista)
-                 (minimo (cdr lista))))))
+    (else (mayor (car lista)
+                 (maximo (cdr lista))))))
+
+;; Demostracion
+(display "\n\nActividad 1")
+(display "\nmaximo '(9 8 6 4 3) => ")
+(display (maximo '(9 8 6 4 3)))
+;; Pruebas
+(check-eq?  (maximo '(1 -1 3 6 4)) 6)
+
+
+;----------------------------Actividad 2
+(define (pertenece? elem l)
+  (cond
+    ((null? l) #f)
+    ((eq? (car l) elem) #t)
+    (else (pertenece? elem (cdr l)))))
+
+(define (repetidos? l)
+  (cond
+    ((null? l) #f)
+    ((pertenece? (car l) (cdr l)) #t)
+    (else (repetidos? (cdr l)))))
 
 ;; Demostracion
 (display "\n\nActividad 2")
-(display "\nminimo '(9 8 6 4 3) => ")
-(display (minimo '(9 8 6 4 3)))
+(display "\nrepetidos? '(1 2 3 5 4 5 6) => ")
+(display (repetidos? '(1 2 3 5 4 5 6)))
 ;; Pruebas
-(check-eq?  (minimo '(-9 0 3 -6 4)) -9)
+(check-eq? (repetidos? '(adios hola que tal)) #f)
+(check-eq? (repetidos? '(#t #f #t #t #t)) #t)
 
-; -----------------------ORDEN APLICATIVO
-;(mayor (+ 2 3) (menor (- 10 2) (* 2 2)))
-;(mayor 5 (menor 8 4))
-;(mayor 5 (if (< 8 4) 8 4))
-;(mayor 5 4)
-;(if (> 5 4) 5 4)
-;5
 
-; ---------------------------ORDEN NORMAL
-;(mayor (+ 2 3) (menor (- 10 2) (* 2 2)))
-;
-;(if (> (+ 2 3) (menor (- 10 2) (* 2 2)))
-;    (+ 2 3)
-;    (menor (- 10 2) (* 2 2)))
-;
-;(if (> (+ 2 3) (menor (- 10 2) (* 2 2)))
-;    (+ 2 3)
-;    (menor (- 10 2) (* 2 2)))
-;
-;(if (> (+ 2 3) (if (< (- 10 2) (* 2 2)) (- 10 2) (* 2 2)))
-;    (+ 2 3)
-;    (if (< (- 10 2) (* 2 2)) (- 10 2) (* 2 2)))
-;
-;(if (> 5 (if (< 8 4) 8 4)) 5 (if (< 8 4) 8 4))
-;
-;(if (> 5 4) 5 4)
-;
-;5
-
-;----------------------------Actividad 3
+;----------------------------Actividad 4
 (define (binario-a-decimal l)
   (cond
     ((null? (cdr l)) (* (car l) (expt 2 0)))
@@ -76,46 +55,33 @@
         (binario-a-decimal (cdr l))))))
 
 ;; Demostracion
-(display "\n\nActividad 3")
+(display "\n\nActividad 4")
 (display "\nbinario-a-decimal '(1 1 1 1) => ")
 (display (binario-a-decimal '(1 1 1 1)))
 ;; Pruebas
 (check-eq?  (binario-a-decimal '(1 1 0)) 6)
 (check-eq?  (binario-a-decimal '(1 0)) 2)
 
-;----------------------------Actividad 4
-(define (member? elem l)
-  (cond
-    ((null? l) #f)
-    ((eq? (car l) elem) #t)
-    (else (member? elem (cdr l)))))
-
-(define (repetidos? l)
-  (cond
-    ((null? l) #f)
-    ((member? (car l) (cdr l)) #t)
-    (else (repetidos? (cdr l)))))
-
-;; Demostracion
-(display "\n\nActividad 4")
-(display "\nrepetidos? '(1 2 3 5 4 5 6) => ")
-(display (repetidos? '(1 2 3 5 4 5 6)))
-;; Pruebas
-(check-eq? (repetidos? '(adios hola que tal)) #f)
-(check-eq? (repetidos? '(#t #f #t #t #t)) #t)
 
 ;----------------------------Actividad 5
-(define (adyacentes-iguales? l)
+(define (ordenada-creciente? l)
   (cond
-    ((null? (cdr l)) #f)
-    ((equal? (car l) (cadr l)) #t)
-    (else (adyacentes-iguales? (cdr l)))))
+    ((null? (cdr l)) #t)
+    ((> (car l) (cadr l)) #f)
+    (else (ordenada-creciente? (cdr l)))))
 
 ;; Demostracion
 (display "\n\nActividad 5")
-(display "\nrepetidos? '(12 30 5  5 2) => ")
-(display (adyacentes-iguales? '(12 30 5  5 2)))
+(display "\nordenada-creciente? '(-1 23 45 59 99) => ")
+(display (ordenada-creciente? '(-1 23 45 59 99)))
+
 ;; Pruebas
-(check-equal? (adyacentes-iguales? '("esto" "es" "una" "lista" "de" "strings")) #f)
-(check-equal? (adyacentes-iguales? '(12 30 #t #\a 5 #f #f)) #t)
-(check-equal? (adyacentes-iguales? (list (cons 1 2) (cons 1 2) (cons 3 4))) #t)
+(check-eq?  (ordenada-creciente? '(12 50 -1 293 1000)) #f)
+(check-eq?  (ordenada-creciente? '(3)) #t)
+
+
+
+
+
+
+
