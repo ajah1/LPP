a00b1d29a0af6d5cfb5b7c24a1697a0f08759162
ALEJANDRO JESUS ALIAGA HYDER <ajah1@clL02-4>
2018-03-01 12:27:34 +0100

    p04 falta 4(B)

--- p04.rkt
+++ p04.rkt
@@ -30,13 +30,70 @@
 
 
 ;--------------------- Actividad 2
+;;(A)
+(define (resultado p)
+  (cond
+    ((> (car p) (cdr p)) 1)
+    ((< (car p) (cdr p)) 2)
+    (else 'X)))
 
-;(resultados‐quiniela lista‐parejas)
-;(cuenta-resultados resultado lista-resultados)
-;(cuenta-resultados-lista lista-resultados) de la práctica 3.
+(define (resultados‐quiniela lp)
+  (map resultado lp))
+
+;;(B)
+(define (cuenta-resultados r lp)
+  (length (filter (lambda (e) (equal? r e))
+                  (resultados‐quiniela lp))))
+
+;;(C)
+; auxiliares para incrementar la lista que cuenta resultados
+(define (inc-gana l)
+  (list (+ 1 (car l)) (cadr l) (caddr l)))
+(define (inc-pierde l)
+  (list (car l) (cadr l) (+ 1 (caddr l))))
+(define (inc-empate l)
+  (list (car l) (+ 1 (cadr l)) (caddr l)))
+
+(define (cuenta dato resultado)
+  (cond
+    ((equal? dato 1) (inc-gana resultado))
+    ((equal? dato 2) (inc-pierde resultado))
+    (else (inc-empate resultado))))
+
+(define (cuenta-resultados-lista lr)
+  (fold-right cuenta (list 0 0 0) (resultados‐quiniela lr)))
+  
+;--------------------- Actividad 3
+(define l3 '((2 . 7) (3 . 5) (10 . 4) (5 . 5)))
+
+;;(A)
+(define a3
+  (fold-right list 0 '(1 2 3)))
+;;(B)
+(define b3
+  (fold-left list "hola" '("como" "estas")))
+;;(C)
+(define c3
+  (filter even?
+          (map (lambda (x) (+ (car x)
+                              (cdr x)))
+               l3)))
+;;(D)
+(define d3
+  (filter (lambda (x) (> (car x) (cdr x)))
+          (map (lambda (x) (cons (cdr x) (car x)))
+               l3)))
+;;(E)
+(define e3
+  (fold-right (lambda (d r) (if (= d 0) r (cons d r)))
+              '()
+              (map (lambda (x) (if (even? (+ (car x)
+                                             (cdr x)))
+                                   (car x)
+                                   0))
+                   l3)))
 
 ;-------------------- Actividad 4
-
 ;;(A)
 (define (make-multiplicador k)
   (lambda (x)
@@ -44,6 +101,8 @@
 
 (define (make-exponenciador e)
   (lambda (b) (expt b e)))
+
+;;(B)
 
 ;-------------------- Actividad 5
 
@@ -54,6 +113,9 @@
        lp))
 
 ;;(B)
+(define (aplica-2 func lp)
+  (map (lambda (p) (func (car p) (cdr p)))
+       lp))
 
 ;-------------------- Actividad 6
 
